4081ca440e4ffbbed70c8d2da732d1dd89ae4b34
Badal Agarwal <63859255+badal-ag@users.noreply.github.com>
2022-02-04 13:10:04 +0530

    Personal API Keys Deleted

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJaQLT0hMkezfwDCpFa3khKIKSd-Vq554",
-    authDomain: "crud-bad.firebaseapp.com",
-    projectId: "crud-bad",
-    storageBucket: "crud-bad.appspot.com",
-    messagingSenderId: "553347574281",
-    appId: "1:553347574281:web:11c1ee82a5276042dae6d8",
-    measurementId: "G-CC09P8B3YF"
+    //Your Firebase API Keys
 }
 
 const app = initializeApp(firebaseConfig);
